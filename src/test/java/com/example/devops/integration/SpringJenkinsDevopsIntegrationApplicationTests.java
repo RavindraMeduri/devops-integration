c2fa9a2972d45e7342d4ed9e3db071a8606f3ffe
RavindraMeduri <nagavenkat6070@gmail.com>
2022-06-25 20:43:57 -0500

    pushing files onto main branch

--- src/test/java/com/example/devops/integration/SpringJenkinsDevopsIntegrationApplicationTests.java
+++ src/test/java/com/example/devops/integration/SpringJenkinsDevopsIntegrationApplicationTests.java
@@ -6,7 +6,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-
 @SpringBootTest
 class SpringJenkinsDevopsIntegrationApplicationTests {
 
